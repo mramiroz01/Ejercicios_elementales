73f97e54bad32490958ffb413492c059dea81864
mramiroz01 <manuel.ramirozabaleta@tiernoparla.es>
2022-09-28 17:47:31 +0200

    delete

---
+++ Ejercicios_elementos/src/main/java/com/tiernoparla/ejercicios_elementos/Ejercicios_elementales.java
@@ -0,0 +1,16 @@
+package com.tiernoparla.ejercicios_elementos;
+
+public class Ejercicios_elementales {
+    
+    public static void main(String[] args) {
+        int D = 10;
+        int d = 5;
+        int c;
+        int r;
+        
+        c = D / d;
+        r = D % d;
+        System.out.println(c);
+        System.out.println(r);
+    }
+}
