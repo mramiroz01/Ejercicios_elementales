d923776aea22813e7356ee35bebae10e7d191c3d
mramiroz01 <manuel.ramirozabaleta@tiernoparla.es>
2022-09-28 17:01:02 +0200

    primer commitç

---
+++ Ejercicios_elementos/src/main/java/com/tiernoparla/ejercicios_elementos/pares.java
@@ -0,0 +1,14 @@
+package com.tiernoparla.ejercicios_elementos;
+
+public class pares {
+    public static void main(String[] args) {
+        int i = 0;
+        
+        while (i <= 10)
+        {
+            if (i % 2 == 0)
+                System.out.println(i);
+            i++;
+        }
+    }
+}
---
+++ src/main/java/com/tiernoparla/ejercicios_elementales/main.java
@@ -0,0 +1,23 @@
+
+package com.tiernoparla.ejercicios_elementales;
+
+public class main {
+    public static void main(String[] args) {
+        int i;
+        
+        i = 0;
+        while (i <= 10)
+        {
+            if (i % 2 == 0)
+                System.out.println(i);
+            i++;
+        }
+        System.out.println("########################");
+        for (int n = 0; n <= 10; n++)
+        {
+            if (n % 2 == 0)
+                System.out.println(n);
+        }
+    }
+    
+}
